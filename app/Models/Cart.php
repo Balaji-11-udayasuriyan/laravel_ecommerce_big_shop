67ac3e72000ab51708450210964f65ace9f2f9ac
Balaji-11-udayasuriyan <balajiudayasuriyan1102@gmail.com>
2024-12-22 12:07:51 +0530

    Create Cart model and migration

---
+++ app/Models/Cart.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Cart extends Model
+{
+    protected $fillable  =  [
+
+        'user_id',
+        'product_id',
+        'qty'
+    ];
+
+    public function user()
+    {
+
+        return $this->belongsTo(User::class,'user_id');
+
+    }
+    public function product()
+    {
+
+        return $this->belongsTo(Product::class,'product_id');
+
+    }
+    public function totalPrice()
+    {
+
+        return $this->qty * $this->product->price;
+
+    }
+    
+    public static function grandTotal($customerId)
+    {
+        
+        $cartItems = Cart::where('customer_id', $customerId)->get();
+
+        $total = $cartItems->sum(function ($item) {
+
+            return $item->totalPrice();
+
+        });
+
+        return $total;
+    }
+}
